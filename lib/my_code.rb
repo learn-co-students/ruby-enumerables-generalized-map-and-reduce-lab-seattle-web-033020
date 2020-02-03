a0ea0e3d41bec372847468c49c31e940010f7c08
John Souza <souzajohnlee@gmail.com>
2020-02-03 21:45:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(array)
+  i = 0
+  while i < array.length do
+    array[i] = yield(array[i])
+    i += 1
+  end
+  array
+end
+
+def reduce(array, value=nil)
+  i = 0
+  if value
+    i = 0
+  else
+    value = array[i]
+    i = 1
+  end
+  while i < array.length do
+    value = yield(value, array[i])
+    i += 1
+  end
+  value
+end
